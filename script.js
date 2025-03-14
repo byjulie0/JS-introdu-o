840427785493f8603d7d58007cc80d38ad5cacd6
byjulie0 <julievieiramartins0@gmail.com>
2025-03-14 16:55:56 -0400

    Realizando pequenas operações

---
+++ script.js
@@ -0,0 +1,36 @@
+// alert("Olá Mundo!!!");
+// // window.confirm("Confirma a conclusão?");
+// window.prompt ("Digite seu mood hoje");
+// var - variavel global, posso usar ela em outro lugar .
+// let - variavel local, so funciona naquele bloco.
+// const - não muda.
+
+// let x = BigInt('12345678906789');
+// let y = 10/3 ;
+
+// let z = y.toFixed(2);
+
+// let w = z.replace(".",",");
+
+// let nome = "Julie";
+
+// console.log (teste?verdadeiro:falso);
+// var media = 8;
+// media>= 7 ? 'Aprovado':'Reprovado';
+
+// let text = "wertyuiojkbnklç";
+// let tamanho = text.length;
+// let text = "Hello Word";
+// let char = text.charAt(0);
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7,13);
+// let text1 = "Hello Word";
+// let text2 = text1.toUpperCase();
+
+// idade = parseInt(prompt("Qual sua idade"));
+// alario = parseFloat(prompt("Qual seu salario"));
+
+// function myFunction(p1,p2){
+//     return p1*p2;
+// }
+// console.log(myFunction(10,20));
